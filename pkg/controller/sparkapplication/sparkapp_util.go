3658dfadb2f8e5178f2752610b4cf5ce1f124324
cytar <cytar@infradvisor.fr>
2020-03-26 09:16:38 +0100

    add sparkConf Parameters

--- pkg/controller/sparkapplication/sparkapp_util.go
+++ pkg/controller/sparkapplication/sparkapp_util.go
@@ -71,7 +71,7 @@ func getResourceLabels(app *v1beta2.SparkApplication) map[string]string {
 	return labels
 }
 
-func getResourceForceSSL(app *v1beta2.SparkApplication) map[string]string {
+func getResourceAnnotations(app *v1beta2.SparkApplication) map[string]string {
 	annotations := map[string]string{}
 	if app.Spec.SparkConf["spark.ssl.ui.enabled"] == "true" {
 		if app.Spec.SparkConf["spark.ui.ingress.class"] == "nginx" {
